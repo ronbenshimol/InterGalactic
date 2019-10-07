88b0c1e5e6fae6790e335801b0b26a8aa953bcd6
Shaked Cohen <shkcohen@gmail.com>
2019-10-07 19:30:58 +0300

    added space fly

---
+++ Assets/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();        
+    }
+
+    void processInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A)){
+            print("");
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            print("");
+        }
+    }
+}
