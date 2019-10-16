ca2ee2f0083118c1cbd946b042e4274f0759f923
Shaked Cohen <shkcohen@gmail.com>
2019-10-16 16:56:45 +0300

    solved git problems! added my two precious levels

--- Assets/States.cs
+++ Assets/States.cs
@@ -6,7 +6,7 @@ using UnityEditor;
 [InitializeOnLoad]
 public static class States
 {
-    public static int InitialLives = 2;
+    public static int InitialLives = 9;
     public static int Lives {get;set;}
     static States(){
         Lives = InitialLives;
